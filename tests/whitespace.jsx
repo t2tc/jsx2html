0cc611ee80a99a3c469c01c0b8ea4280542dba91
t2tc <61874231+t2tc@users.noreply.github.com>
2024-11-04 11:24:52 +0800

    whitespace test.

---
+++ tests/whitespace.jsx
@@ -0,0 +1,14 @@
+
+const a = <div>
+    Hello
+    World
+</div>
+
+const b = <p>        Hello      World       </p>;
+
+const c = <div>
+    <p>Hello</p>
+    <p>World</p>
+</div>
+
+console.log(a);
